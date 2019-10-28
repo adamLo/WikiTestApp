baa9dcd0929168a2d0b55efc5c1fd05266f520ea
Adam Lovastyik <adam@lovastyik.hu>
2019-10-28 20:05:59 +0100

    Added unit test for CustomLocation

--- WikiTestAppTests/WikiTestAppTests.swift
+++ WikiTestAppTests/WikiTestAppTests.swift
@@ -18,17 +18,74 @@ class WikiTestAppTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // MARK: - Test Initialization with a string
+    
+    func testCustomLocationInitValid() {
+        
+        let location = CustomLocation(coordinates: "12,34")
+        XCTAssertNotNil(location)
+        XCTAssertEqual(location?.coordinate.latitude, 12)
+        XCTAssertEqual(location?.coordinate.longitude, 34)
+    }
+    
+    func testCustomLocationInitFailNil() {
+        
+        let location = CustomLocation(coordinates: nil)
+        XCTAssertNil(location)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCustomLocationInitFailEmptyString() {
+        
+        let location = CustomLocation(coordinates: "")
+        XCTAssertNil(location)
     }
 
+    func testCustomLocationInitFailInvalidString() {
+        
+        let location = CustomLocation(coordinates: "wrong")
+        XCTAssertNil(location)
+    }
+    
+    func testCustomLocationInitFailNotEnoghCoords() {
+        
+        let location = CustomLocation(coordinates: "1")
+        XCTAssertNil(location)
+    }
+    
+    func testCustomLocationInitFailTooManyCoords() {
+        
+        let location = CustomLocation(coordinates: "1,2,3")
+        XCTAssertNil(location)
+    }
+    
+    func testCustomLocationInitFailNonNumericCoords() {
+        
+        let location = CustomLocation(coordinates: "a,b")
+        XCTAssertNil(location)
+    }
+    
+    func testCustomLocationInitFailInvalidCoords() {
+        
+        let location = CustomLocation(coordinates: "400,500")
+        XCTAssertNil(location)
+    }
+    
+    // MARK: - Test URL construction
+    
+    func testURLConstruction() {
+        
+        let location = CustomLocation(coordinates: "12,34")
+        XCTAssertNotNil(location)
+        
+        let url = location?.wikiURL
+        XCTAssertNotNil(url)
+        XCTAssertEqual(url?.host, "places")
+        
+        let components = URLComponents(url: url!, resolvingAgainstBaseURL: false)
+        XCTAssertNotNil(components)
+        XCTAssertEqual(components?.queryItems?.count, 1)
+        XCTAssertEqual(components?.queryItems?.first?.name, "LACoordinate")
+        XCTAssertEqual(components?.queryItems?.first?.value, "12.0,34.0")
+    }
 }
